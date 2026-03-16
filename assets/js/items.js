01e6bb72218ef0e9ea7541257ef0f6f7260650ef
joono1004 <ljhs1004@gmail.com>
2026-03-17 07:41:59 +0900

    Create items.js

---
+++ assets/js/items.js
@@ -0,0 +1,45 @@
+export const ITEM_DB = [
+
+/* =======================
+   무기
+======================= */
+
+{ id:"war_dagger", name:"단검", class:"전사", slot:"weapon", atk:3 },
+
+{ id:"war_longsword", name:"장검", class:"전사", slot:"weapon", atk:6 },
+
+{ id:"war_knight_sword", name:"기사검", class:"전사", slot:"weapon", atk:10 },
+
+{ id:"arc_shortbow", name:"단궁", class:"궁수", slot:"weapon", atk:3 },
+
+{ id:"arc_longbow", name:"장궁", class:"궁수", slot:"weapon", atk:6 },
+
+{ id:"arc_crossbow", name:"석궁", class:"궁수", slot:"weapon", atk:10 },
+
+{ id:"mag_staff_wood", name:"나무 지팡이", class:"마법사", slot:"weapon", atk:3 },
+
+{ id:"mag_staff", name:"마법 지팡이", class:"마법사", slot:"weapon", atk:6 },
+
+{ id:"mag_staff_crystal", name:"수정 지팡이", class:"마법사", slot:"weapon", atk:10 },
+
+/* =======================
+   방패
+======================= */
+
+{ id:"war_shield_wood", name:"나무 방패", class:"전사", slot:"shield", def:3 },
+
+{ id:"war_shield_round", name:"원형 방패", class:"전사", slot:"shield", def:6 },
+
+{ id:"war_shield_steel", name:"강철 방패", class:"전사", slot:"shield", def:10 },
+
+/* =======================
+   망토
+======================= */
+
+{ id:"cloak_merc", name:"용병 망토", class:"all", slot:"cloak", def:2 },
+
+{ id:"cloak_knight", name:"기사 망토", class:"all", slot:"cloak", def:4 },
+
+{ id:"cloak_hero", name:"용사 망토", class:"all", slot:"cloak", def:6 }
+
+];
