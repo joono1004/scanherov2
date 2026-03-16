972d766bf95a0881a50de3f27ce0a07d19bc9a95
joono1004 <ljhs1004@gmail.com>
2026-03-17 07:42:28 +0900

    Create rarity.js

---
+++ assets/js/rarity.js
@@ -0,0 +1,13 @@
+export const RARITY_TABLE = {
+
+common:   { name:"일반", mult:1.0, color:"#a9b6d6" },
+
+uncommon: { name:"고급", mult:1.3, color:"#63d09a" },
+
+rare:     { name:"희귀", mult:1.7, color:"#63a8ff" },
+
+epic:     { name:"영웅", mult:2.3, color:"#b27dff" },
+
+legendary:{ name:"전설", mult:3.2, color:"#ffcf63" }
+
+};
